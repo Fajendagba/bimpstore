f262eeef8260af63151663773ec22e8534b5d26a
joy <joy.fajendagba@archwaretechgroup.com>
2023-03-18 22:36:11 +0100

    initial commit

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'bimps-store') or 
+        die('Could not connect due to: '. mysqli_connect_error());
+
+?>
---
+++ css/bimpsStyle.php
@@ -0,0 +1,18 @@
+<!-- FontAwesome -->
+<link rel="stylesheet" href="fontawesome-free-5.1.0-web/css/all.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+<style>
+#console {
+    position: absolute;
+    top: 52px;
+    left: -70px;
+    width: 30%;
+    height: 500px;
+}
+    
+#console #pre {
+    height: 500px;
+    overflow-y: scroll;
+}
+</style>
